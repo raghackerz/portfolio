1a94c847d14f514c7c4b86668fe021ac084769cc
Raghvenda Kumar Singh <raghvendra14fb@gmail.com>
2022-06-23 23:20:03 +0530

    added the background component to display the background

---
+++ src/components/Background/Background.styles.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: hsl(240 40% 20%);
+  background: hsla(239, 50%, 10%, 1);
+background: linear-gradient(hsla(239, 50%, 15%, 1),hsla(239, 50%, 0%, 1))
+`;
+export const Circle = styled.div.attrs(props => ({
+  style: {
+    top: `calc(50vh / (${props.speed} * 10) - ${props.pointerPositionY}px / (${props.speed} * 10) + 50vh - (${props.size} / 2))`,
+    left: `calc(50vw / (${props.speed} * 10) - ${props.pointerPositionX}px / (${props.speed} * 10) + 25vw + 50vw - ${props.size} / 2)`
+  }
+}))`
+  position: fixed;
+  border-radius: 50%;
+  background-color: hsl(0 100% 100% / 0.0${props => props.child});
+  width: ${props => props.size};
+  height: ${props => props.size};
+  
+`;
+
+export const ImageWrapper = styled.div `
+  img:nth-child(1) {
+    filter: url(#transmissionerror);
+  }
+
+  img:nth-child(2) {
+    filter: grayscale(100%) contrast(1000%);
+    mix-blend-mode: multiply;
+  }
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  transform: scale(0.8);
+`;
+
+export const SmallCircle1 = styled.div.attrs(props => ({
+  style: {
+    top: `calc(50vh / 500 - ${props.pointerPositionY}px / 500 + ${props.top}`,
+    left: `calc(50vw / 500 - ${props.pointerPositionX}px / 500 + ${props.left}`,
+  }
+}))`
+  position: fixed;
+  border-radius: 50%;
+  width: 3px;
+  height: 3px;
+  background: hsl(0 100% 100% / 0.2);
+`;
+export const SmallCircle2 = styled.div.attrs(props => ({
+  style: {
+    top: `calc(50vh / 60 - ${props.pointerPositionY}px / 60 + ${props.top}`,
+    left: `calc(50vw / 60 - ${props.pointerPositionX}px / 60 + ${props.left}`,
+  }
+}))`
+  position: fixed;
+  border-radius: 50%;
+  width: 4px;
+  height: 4px;
+  background: hsl(0 100% 100% / 0.3);
+`;
+export const SmallCircle3 = styled.div.attrs(props => ({
+  style: {
+    top: `calc(50vh / 300 - ${props.pointerPositionY}px / 300 + ${props.top}`,
+    left: `calc(50vw / 300 - ${props.pointerPositionX}px / 300 + ${props.left}`,
+  }
+}))`
+  position: fixed;
+  border-radius: 50%;
+  width: 3px;
+  height: 3px;
+  background: hsl(0 100% 100% / 0.4);
+`;
---
+++ src/components/Background/index.js
@@ -0,0 +1,47 @@
+import React, { useState, /*useEffect*/ } from 'react';
+
+//Styles
+import { Wrapper, Circle, ImageWrapper, Image,} from './Background.styles';
+
+//components
+import Stars from '../Stars';
+
+//resources
+import IMG from '../../skull1.png';
+
+
+const Background = () => {
+  const [pointerPosition, setPointerPosition] = useState([window.innerWidth/2,window.innerHeight/2]);
+  return (
+      <Wrapper onMouseMove={(e)=>setPointerPosition([e.clientX,e.clientY])} >
+        <Circle size='50vw' child='1' pointerPositionY={pointerPosition[1]} pointerPositionX={pointerPosition[0]} speed={2}/>
+        <Circle size='45vw' child='2' pointerPositionY={pointerPosition[1]} pointerPositionX={pointerPosition[0]} speed={2}/>
+        <Circle size='40vw' child='3' pointerPositionY={pointerPosition[1]} pointerPositionX={pointerPosition[0]} speed={2}/>
+        <Circle size='35vw' child='4' pointerPositionY={pointerPosition[1]} pointerPositionX={pointerPosition[0]} speed={2}/>
+        <Circle size='30vw' child='5' pointerPositionY={pointerPosition[1]} pointerPositionX={pointerPosition[0]} speed={2}/>
+        <Circle size='25vw' child='6' pointerPositionY={pointerPosition[1]} pointerPositionX={pointerPosition[0]} speed={2}/>
+        <Circle size='20vw' child='7' pointerPositionY={pointerPosition[1]} pointerPositionX={pointerPosition[0]} speed={2}>
+          <ImageWrapper>
+            <Image src={IMG} draggable="false" />
+            <svg>
+              <filter id="transmissionerror">
+                <feColorMatrix type="saturate" values="0" in="SourceGraphic" result="colormatrix1" />
+                <feColorMatrix type="matrix" values="0.95 0 0 0 0  0 0 0 0 0  0 0 0.4 0 0  0 0 0 1 0" in="colormatrix1" result="colormatrix2" />
+                <feTurbulence type="turbulence" baseFrequency="0.002 0.008" numOctaves="2" seed="5" stitchTiles="noStitch" result="turbulence">
+                  <animate attributeName="baseFrequency" from="0.002 0.008" to="0.002 0.01" dur="3500ms" repeatCount="indefinite" />
+                  <animate attributeName="numOctaves" from="1" to="10" dur="750ms" repeatCount="indefinite" />
+                  <animate attributeName="seed" from="1" to="100" dur="10s" repeatCount="indefinite" />
+                </feTurbulence>
+                <feColorMatrix type="saturate" values="30" in="turbulence" result="colormatrix3" />
+                <feColorMatrix type="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 100 -15" in="colormatrix3" result="colormatrix4" />
+                <feDisplacementMap in="colormatrix2" in2="colomatrix4" scale="15" xChannelSelector="R" yChannelSelector="A" result="displacementMap" />
+              </filter>
+            </svg>
+          </ImageWrapper>
+        </Circle>
+        <Stars pointerPosition={pointerPosition}/>
+      </Wrapper>
+  );
+};
+
+export default Background;
