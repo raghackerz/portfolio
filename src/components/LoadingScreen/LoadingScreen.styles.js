db8788d7f2a703e77c1eacc19069558e248e2e66
Raghvenda Kumar Singh <raghvendra14fb@gmail.com>
2022-06-23 23:15:57 +0530

    updated the loading screen

--- src/components/LoadingScreen/LoadingScreen.styles.js
+++ src/components/LoadingScreen/LoadingScreen.styles.js
@@ -1,57 +1,146 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Spinner = styled.div`
+export const Wrapper = styled.div`
+  --size: 200px;
+  --duration: 0.5s;
+  /*--delay: 10s;*/
+  position: fixed;
+  top: 0;
   background-color: hsl(240,15%,15%);
   width: 100vw;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-.wave {
-  width: 5px;
-  height: 100px;
-  background: linear-gradient(45deg, cyan, #fff);
-  margin: 10px;
-  animation: wave 1s linear infinite;
-  border-radius: 20px;
-}
-.wave:nth-child(2) {
-  animation-delay: 0.1s;
-}
-.wave:nth-child(3) {
-  animation-delay: 0.2s;
-}
-.wave:nth-child(4) {
-  animation-delay: 0.3s;
-}
-.wave:nth-child(5) {
-  animation-delay: 0.4s;
-}
-.wave:nth-child(6) {
-  animation-delay: 0.5s;
-}
-.wave:nth-child(7) {
-  animation-delay: 0.6s;
-}
-.wave:nth-child(8) {
-  animation-delay: 0.7s;
-}
-.wave:nth-child(9) {
-  animation-delay: 0.8s;
-}
-.wave:nth-child(10) {
-  animation-delay: 0.9s;
-}
+  z-index: 2;
 
-@keyframes wave {
+  animation: entry var(--duration) cubic-bezier(.01,1.18,.38,.98);
+
+  ${props =>
+      props.exit ?
+      css`
+        animation: exit var(--duration) ease-in-out;
+        animation-fill-mode: forwards;
+      ` : 
+      css`
+        animation: entry var(--duration) cubic-bezier(.01,1.18,.38,.98);
+      `};
+
+@keyframes entry {
   0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1);
+    left: 100vw;
   }
   100% {
-    transform: scale(0);
+    left: 0vw;
   }
 }
+@keyframes exit {
+  0% {
+    left: 0vw;
+  }
+  100% {
+    left: -100vw;
+  }
+}
+@media (max-width: 768px) {
+  --size: 120px;
+}
+`;
+
+export const Spinner = styled.div`
+
+  display: block;
+  position: absolute;
+  top: calc(50% - var(--size) / 2);
+  left: calc(50% - var(--size) / 2);
+  height: var(--size);
+  width: var(--size);
+  margin: -25px 0 0 -25px;
+  border: 2px solid transparent;
+  border-top-color: #FFF;
+  box-shadow: 0 0 5px #FFFFFF;
+  border-radius: 50%;
+  -webkit-animation: spin 1.5s ease infinite;
+          animation: spin 1.5s ease infinite;
+    /*animation-delay: var(--delay);*/
+
+:before {
+  content: "";
+  position: absolute;
+  top: calc(var(--size)/6);
+  right: calc(var(--size)/6);
+  bottom: calc(var(--size)/6);
+  left: calc(var(--size)/6);
+  border: 2px solid transparent;
+  border-radius: 50%;
+  border-top-color: #FFF;
+  box-shadow: 0 0 5px #FFFFFF;
+  -webkit-animation: spin_x 1s linear infinite;
+          animation: spin_x 1s linear infinite;
+    /*animation-delay: var(--delay);*/
+}
+
+:after {
+  content: "";
+  position: absolute;
+  top: calc(var(--size)*30/100);
+  right: calc(var(--size)*30/100);
+  bottom: calc(var(--size)*30/100);
+  left: calc(var(--size)*30/100);
+  border: 2px solid transparent;
+  border-radius: 50%;
+  border-top-color: #FFF;
+  box-shadow: 0 0 5px #FFFFFF;
+  -webkit-animation: spin 1.5s linear infinite;
+          animation: spin 1.5s linear infinite;
+    /*animation-delay: var(--delay);*/
+}
+
+@-webkit-keyframes spin {
+  from {
+    -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+  }
+}
+@keyframes spin {
+  from {
+    -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+    -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+  }
+}
+@-webkit-keyframes spin_x {
+  0% {
+    -webkit-transform: rotateX(0deg);
+            transform: rotateX(0deg);
+  }
+  100% {
+    -webkit-transform: rotateX(360deg);
+            transform: rotateX(360deg);
+  }
+}
+@keyframes spin_x {
+  0% {
+    -webkit-transform: rotateX(0deg);
+            transform: rotateX(0deg);
+    -webkit-transform: rotateX(0deg);
+            transform: rotateX(0deg);
+  }
+  100% {
+    -webkit-transform: rotateX(360deg);
+            transform: rotateX(360deg);
+    -webkit-transform: rotateX(360deg);
+            transform: rotateX(360deg);
+  }
+}
+`;
+export const Image = styled.img`
 `;
--- src/components/LoadingScreen/index.js
+++ src/components/LoadingScreen/index.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import { Spinner } from './LoadingScreen.styles';
+import React, { useState, useEffect } from 'react';
+import { Wrapper, Spinner, Image } from './LoadingScreen.styles';
 
-const LoadingScreen = () => {
+/*const LoadingScreen = ({onClick}) => {
   return (
-      <Spinner>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-      </Spinner>
+    <AnimatePresence exitBeforeEnter>
+    <motion.div
+      key={onClick}
+      style={{position: 'fixed'}}
+      initial={{left: '100vw'}}
+      animate={{left: '0vw'}}
+      exit={{left: '-100vw'}}
+      transition = {{delay: 0.5}}
+    >
+      <Wrapper>
+      <Spinner onClick={onClick}/>
+      </Wrapper>
+    </motion.div>
+    </AnimatePresence>
+  );
+}*/
+const LoadingScreen = ({onClick, loading}) => {
+  const [req, setReq] = useState(true);
+  const [exit,setExit] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
+  const delay = 500;
+  const wait = 200;
+  useEffect(() => {
+    if(!loading) {
+      setTimeout(()=>{setExit(true)},wait,'funky');
+      setTimeout(()=>{setReq(false)},2*delay+wait,'funky');
+    }
+  },[loading]);
+
+  useEffect(() => {
+    setTimeout(()=>{setShowSpinner(true)},1000+delay,'funky');
+  },[]);
+
+  if(!req) {
+    return (<></>);
+  }
+  
+  return (
+      <Wrapper exit={exit}>
+        {showSpinner && 
+          <Spinner onClick={onClick}/>
+        }
+      </Wrapper>
   );
 }
 
