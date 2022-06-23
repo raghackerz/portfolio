bcd04799a1862513307b17c42588e82186ad4ac1
Raghvenda Kumar Singh <raghvendra14fb@gmail.com>
2022-06-23 23:16:55 +0530

    made changes to the global styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,5 +4,13 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    overflow: hidden;
+    font-size: 16px;
+  }
+  img {
+    user-select: none;
+  }
+  .transitionin {
+    animation: exit 0.25s ease-in;
   }
 `;
